fa792699a7e76c4f1d60c974fd65f2fe85942087
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-12-12 18:55:36 -0200

    Update indicator icon

--- data/plugins/indicator_plugin.py
+++ data/plugins/indicator_plugin.py
@@ -1,48 +1,63 @@
 from __future__ import unicode_literals
 
 import logging
+from locale import gettext as _
+
+from gi.repository import AppIndicator3, Gtk
+from wiring import implements
 
 from tomate.constant import State
 from tomate.event import Events, on
 from tomate.graph import graph
 from tomate.plugin import Plugin
 from tomate.utils import suppress_errors
+from tomate.view import TrayIcon
 
 logger = logging.getLogger(__name__)
 
 
+@implements(TrayIcon)
 class IndicatorPlugin(Plugin):
 
     @suppress_errors
     def __init__(self):
         super(IndicatorPlugin, self).__init__()
-        self.indicator = graph.get('tomate.indicator')
+
+        self.view = graph.get('tomate.view')
+        self.config = graph.get('tomate.config')
+
+        menuitem = Gtk.MenuItem(_('Show'), visible=False)
+        menuitem.connect('activate', self.on_show_menu_activate)
+        menu = Gtk.Menu(halign=Gtk.Align.CENTER)
+        menu.add(menuitem)
+
+        menu.show_all()
+
+        self.indicator = AppIndicator3.Indicator.new(
+                'tomate',
+                'tomate-indicator',
+                AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+        )
+
+        self.indicator.set_icon_theme_path(self.config.get_icon_paths()[0])
+        self.indicator.set_menu(menu)
+
+    def on_show_menu_activate(self, widget=None):
+        return self.view.show()
 
     @suppress_errors
     def activate(self):
         super(IndicatorPlugin, self).activate()
-        self.idle_icon()
+        graph.register_instance(TrayIcon, self)
 
     @suppress_errors
     def deactivate(self):
         super(IndicatorPlugin, self).deactivate()
-        self.default_icon()
-
-    @suppress_errors
-    @on(Events.Session, [State.stopped, State.started, State.reset])
-    def default_icon(self, *args, **kwargs):
-        self.indicator.set_icon('tomate-indicator')
-
-        logger.debug('set defaul icon')
-
-    def idle_icon(self, *args, **kwargs):
-        self.indicator.set_icon('tomate-idle')
-
-        logger.debug('set idle icon')
+        graph.unregister_provider(TrayIcon)
 
     @suppress_errors
     @on(Events.Timer, [State.changed])
-    def update_icon(self, *args, **kwargs):
+    def update_icon(self, sender=None, **kwargs):
         percent = int(kwargs.get('time_ratio', 0) * 100)
 
         # The icons show 5% steps, so we have to round
@@ -55,9 +70,11 @@ class IndicatorPlugin(Plugin):
 
             logger.debug('set icon %s', icon_name)
 
-    @suppress_errors
-    @on(Events.Session, [State.finished])
-    def attention_icon(self, *args, **kwargs):
-        self.indicator.set_icon('tomate-attention')
+    @on(Events.View, [State.hiding])
+    def show(self, sener=None, **kwargs):
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 
-        logger.debug('set attention icon')
+    @on(Events.Session, [State.finished])
+    @on(Events.View, [State.showing])
+    def hide(self, sender=None, **kwargs):
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.PASSIVE)
